fa4badd0ef16dfaac35714a6368cf7a4eb0fd93c
Faith Kilonzi <fkilonzi@webtribe.co.ke>
2017-06-29 12:17:02 +0300

    yii files here

---
+++ frontend/views/site/entry.php
@@ -0,0 +1,29 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+?>
+
+<?php
+    if(Yii::$app->session->hasFlash("success")){
+        echo Yii::$app->session->getFlash("success");        
+    }
+?>
+<?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name') ?>
+
+    <?= $form->field($model, 'email') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+    </div>
+
+<?php ActiveForm::end(); ?>
